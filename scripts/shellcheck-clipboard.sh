9175c5a3891add25f202feb7f1e96d5268f52892
Davide Di Criscito <davide.dicriscito@gmail.com>
2025-05-29 05:12:56 +0200

    chore: fix make lint command

--- scripts/shellcheck-clipboard.sh
+++ scripts/shellcheck-clipboard.sh
@@ -3,4 +3,4 @@
 # Checks for errors inside the bin, scripts, and devtoolkit only, and only f or sh files
 # copies output to clipboard
 
-find ./bin ./scripts ./devtoolkit -type f -name '*.sh' -exec shellcheck -x {} + | xclip -selection clipboard && prinff "\n✅ Done"
+find ./bin ./scripts ./devtoolkit -type f -name '*.sh' -exec shellcheck -x {} + | xclip -selection clipboard && printf "\n✅ Done\n"
